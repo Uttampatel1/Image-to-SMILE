9cf1dae1ab2090023760d802e07717a1ac6037ec
Uttam Patel <86312108+Uttampatel1@users.noreply.github.com>
2024-02-13 11:28:57 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,41 @@
+import os
+import streamlit as st
+from DECIMER import predict_SMILES
+from PIL import Image
+from io import BytesIO
+import tempfile
+
+# Function to predict SMILES
+def predict_smiles(image_path):
+    SMILES = predict_SMILES(image_path)
+    return SMILES
+
+# Main function for Streamlit app
+def main():
+    st.title("SMILES Prediction from PNG Images")
+    st.write("ℹ️ Upload PNG images and let me predict the SMILES notation for each one!")
+
+    uploaded_files = st.file_uploader("📂 Upload PNG Images", type="png", accept_multiple_files=True)
+    
+    if uploaded_files:
+        for uploaded_file in uploaded_files:
+            # Save uploaded file to temporary directory
+            temp_dir = tempfile.mkdtemp()
+            temp_image_path = os.path.join(temp_dir, "uploaded_image.png")
+            with open(temp_image_path, "wb") as f:
+                f.write(uploaded_file.getvalue())
+
+            # Read and display uploaded image
+            image = Image.open(temp_image_path)
+            st.image(image, caption=f'🖼️ Uploaded Image - {uploaded_file.name}', width=400)
+
+            with st.spinner("⌛ Creating SMILES..."):
+                # Predict SMILES
+                SMILES = predict_smiles(temp_image_path)
+            
+            st.write(f"🔍 Predicted SMILES for {uploaded_file.name}:")
+            st.write(SMILES)
+            st.write("---")
+
+if __name__ == "__main__":
+    main()
